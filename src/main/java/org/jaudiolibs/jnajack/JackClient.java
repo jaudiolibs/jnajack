b2ad91869851f704dd4b72a987cd82724f40ce6e
David Zhang <zdl@zdl.hk>
2014-03-07 02:04:45 +0800

    bug fix

--- src/main/java/org/jaudiolibs/jnajack/JackClient.java
+++ src/main/java/org/jaudiolibs/jnajack/JackClient.java
@@ -537,7 +537,6 @@ public class JackClient {
             this.callback = callback;
         }
 
-        @Override
         public void invoke(int a, int b, int connect, Pointer arg) {
             try {
                 _jack_port pA = jackLib.jack_port_by_id(clientPtr, a);
@@ -566,7 +565,6 @@ public class JackClient {
             this.callback = callback;
         }
 
-        @Override
         public void invoke(ByteByReference name, int register, Pointer arg) {
             try {
                 String nameString = name.getPointer().getString(0);
@@ -590,7 +588,6 @@ public class JackClient {
             this.callback = callback;
         }
 
-        @Override
         public void invoke(int port, int int1, Pointer arg) {
             try {
 
@@ -616,7 +613,6 @@ public class JackClient {
             this.callback = callback;
         }
 
-        @Override
         public int invoke(Pointer arg) {
             callback.graphOrderChanged(JackClient.this);
             return 0;
